048e645db2a25f7013a0b77d6b52698b79f5739e
Dhanush-Abhiram <dhanush.sr03@gmail.com>
2025-01-05 01:21:02 -0500

    Update vite config and index.html

--- --/vite.config.js
+++ --/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // Ensure this matches your repository name
+  base: '/Portfolio/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-});
+    assetsDir: 'assets'
+  }
+})
