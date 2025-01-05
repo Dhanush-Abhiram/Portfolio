6f35e63c617f3c975154e0cae125b519f9c83047
Dhanush-Abhiram <dhanush.sr03@gmail.com>
2025-01-05 18:31:17 -0500

    Updated portfolio project

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio/',  // This ensures correct asset linking for GitHub Pages
   plugins: [react()],
   build: {
+    outDir: 'dist',  // This is where the build will be placed
     rollupOptions: {
       input: {
         main: './index.html',
       },
     },
   },
-})
+});
